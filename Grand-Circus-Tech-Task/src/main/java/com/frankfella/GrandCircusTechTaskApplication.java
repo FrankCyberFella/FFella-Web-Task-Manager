84ceef7d36e6ad15389b703238624f6a6b6bb50b
FrankJFella <frank.fella@bloomtech.com>
2023-11-07 09:27:37 -0700

    Instructor Push 11/07/2023 09:27:37

---
+++ Grand-Circus-Tech-Task/src/main/java/com/frankfella/GrandCircusTechTaskApplication.java
@@ -0,0 +1,13 @@
+package com.frankfella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandCircusTechTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandCircusTechTaskApplication.class, args);
+	}
+
+}
---
+++ Grand-Circus-Tech-Task/src/test/java/com/frankfella/GrandCircusTechTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankfella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandCircusTechTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
